c88ba6d6d64c77e10d915e33835fd3553f21170b
Bo Maryniuk <bo@suse.de>
2020-02-13 17:25:01 +0100

    Implement config finder (to kill typical boilerplate)

---
+++ findconf.go
@@ -0,0 +1,94 @@
+/*
+Nanoconf finder helper.
+A convenience class that allows to organise configuration files for the application.
+Configuration files might be found in /etc/somewhere.conf, $HOME/.somewhere,
+$HOME/.config/yourapp/somewhere.conf, /opt/yourapp/etc/somewhere.conf etc etc etc.
+
+To organise all that "mess", NanoconfFinder is to serve.
+*/
+
+package nanoconf
+
+import (
+	"os"
+	"os/user"
+	"path"
+)
+
+type NanoconfFinder struct {
+	appname string
+	names   []string
+	paths   []string
+}
+
+// Constructor
+func NewNanoconfFinder(appname string) *NanoconfFinder {
+	nf := new(NanoconfFinder)
+	nf.appname = appname
+	nf.names = make([]string, 0)
+	nf.paths = make([]string, 0)
+	return nf
+}
+
+// Setup typical locations for the user: /etc, $HOME/.config..., $HOME/.<cfg> etc.
+// If user is nil, current user is used.
+func (nf *NanoconfFinder) DefaultSetup(usr *user.User) *NanoconfFinder {
+	var err error
+	if usr == nil {
+		usr, err = user.Current()
+		if err != nil {
+			panic(err)
+		}
+	}
+	nf.paths = append(nf.paths, []string{"/etc", path.Join("/etc", "appname"), usr.HomeDir, path.Join(usr.HomeDir, ".config", nf.appname), "./"}...)
+	nf.names = append(nf.names, []string{nf.appname + ".conf", "." + nf.appname}...)
+	return nf
+}
+
+// FindFirst finds nearest  configuration that matches the search
+func (nf *NanoconfFinder) FindFirst() string {
+	var pth string
+	config := nf.FindAll()
+	if len(config) > 0 {
+		pth = config[0]
+	}
+	return pth
+}
+
+// FindAll finds all possible configuration files that would match the search.
+func (nf *NanoconfFinder) FindAll() []string {
+	configs := make([]string, 0)
+	for _, pth := range nf.paths {
+		for _, nm := range nf.names {
+			cfgpth := path.Join(pth, nm)
+			if fh, err := os.Stat(cfgpth); os.IsExist(err) && !fh.IsDir() {
+				configs = append(configs, cfgpth)
+			}
+		}
+	}
+	return configs
+}
+
+// CleanPaths removes all the paths from the stack
+func (nf *NanoconfFinder) CleanPaths() *NanoconfFinder {
+	nf.paths = nil
+	return nf
+}
+
+// CleanNames removes all the names from the stack
+func (nf *NanoconfFinder) CleanNames() *NanoconfFinder {
+	nf.names = nil
+	return nf
+}
+
+// AddName adds a name to the stack
+func (nf *NanoconfFinder) AddName(name string) *NanoconfFinder {
+	nf.names = append(nf.names, name)
+	return nf
+}
+
+// AddPath adds a path to the stack
+func (nf *NanoconfFinder) AddPath(cfgpath string) *NanoconfFinder {
+	nf.paths = append(nf.paths, cfgpath)
+	return nf
+}
